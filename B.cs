d3058c8451fce4cff81d113a3438f6d202dbff4d
Milo <srdjan.milojevic@eng.it>
2025-07-17 11:56:38 +0200

    Added new class B

---
+++ B.cs
@@ -0,0 +1,14 @@
+public class B
+{
+    public void MethodB()
+    {
+        Console.WriteLine("Method B");
+    }
+
+    public B(string name)
+    {
+        Console.WriteLine($"Constructor B called with name: {name}");
+        Name = name;
+    }
+    public string Name { get;}
+}
