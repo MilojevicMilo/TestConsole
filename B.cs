bfea3c30c2781d0bd4eca85119364eee599e81e8
Milo <srdjan.milojevic@eng.it>
2025-07-17 14:54:06 +0200

    removed B from name property

--- B.cs
+++ B.cs
@@ -8,7 +8,7 @@ public class B
     public B(string name)
     {
         Console.WriteLine($"Constructor B called with name: {name}");
-        Name = name + "B";
+        Name = name ;
     }
     public string Name { get;}
 }
